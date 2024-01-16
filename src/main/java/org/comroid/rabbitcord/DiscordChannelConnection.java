49af8129d5fbdef20802b242c6a3247a4939e880
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-01-16 18:31:27 +0100

    fixes

--- src/main/java/org/comroid/rabbitcord/DiscordChannelConnection.java
+++ src/main/java/org/comroid/rabbitcord/DiscordChannelConnection.java
@@ -1,18 +1,31 @@
 package org.comroid.rabbitcord;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.Value;
 import lombok.experimental.NonFinal;
+import lombok.extern.java.Log;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import org.comroid.annotations.Alias;
 import org.comroid.annotations.Ignore;
 import org.comroid.api.attr.UUIDContainer;
+import org.comroid.api.data.seri.DataNode;
+import org.comroid.api.func.util.Debug;
 import org.comroid.api.tree.Component;
 
-import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 
+@Log
 @Value
 @RequiredArgsConstructor
 @EqualsAndHashCode(callSuper = true)
@@ -22,15 +35,83 @@ public class DiscordChannelConnection extends Component.Base {
     @NonFinal Channel channel;
 
     @Override
+    @SneakyThrows
     protected void $initialize() {
-        super.$initialize();
+        var factory = new ConnectionFactory();
+        factory.setUri(config.amqpUri);
+        connection = factory.newConnection();
+
+        channel = connection.createChannel();
+        channel.exchangeDeclare("aurion.chat", "fanout");
+
+        String queue = channel.queueDeclare().getQueue();
+        channel.queueBind(queue, "aurion.chat", "");
+
+        channel.basicConsume(queue, true, (consumerTag, content) -> {
+            try {
+                var data = new JsonParser().parse(new String(content.getBody(), StandardCharsets.UTF_8)).getAsJsonObject();
+                var component = GsonComponentSerializer.gson().deserialize(data.get("message").getAsString());
+                if (!data.has("source"))
+                    sendToDiscord(component);
+            } catch (Throwable t) {
+                log.log(Level.SEVERE, "Internal error", t);
+            }
+        }, consumerTag -> {});
+    }
+
+    @Override
+    @SneakyThrows
+    protected void $terminate() {
+        channel.close();
+        connection.close();
+    }
+
+    public String string(net.kyori.adventure.text.Component comp) {
+        if (comp instanceof TextComponent text)
+            return text.content() + comp.children().stream()
+                    .map(this::string)
+                    .collect(Collectors.joining());
+        return comp.toString();
+    }
+
+    public void sendToDiscord(net.kyori.adventure.text.Component component) {
+        RabbitCord.Instance.getJda()
+                .getGuildById(config.guildId)
+                .getTextChannelById(config.channelId)
+                .sendMessage(string(component)
+                        .replaceAll("[§&]\\w", "")
+                +(Debug.isDebug()?"\n\n```json\n"+GsonComponentSerializer.gson().serializeToTree(component)+"\n```":""))
+                .queue();
+    }
+
+    @SneakyThrows
+    public void sendToRabbit(net.kyori.adventure.text.Component component) {
+        var json = new JsonObject();
+
+        json.addProperty("type", "chat");
+        json.addProperty("source", "discord");
+        json.addProperty("channel", "global");
+        json.addProperty("message", GsonComponentSerializer.gson().serializeToTree(component).toString());
+
+        channel.basicPublish(config.exchange, "", null, json.toString().getBytes());
     }
 
     @lombok.Value
-    public static class Config implements UUIDContainer {
+    public static class Config implements DataNode, UUIDContainer {
         long guildId;
         long channelId;
-        URI amqpUri;
+        String amqpUri;
+        String exchange;
+
+        public Config(@Alias("guildId") long guildId,
+                      @Alias("channelId") long channelId,
+                      @Alias("amqpUri") String amqpUri,
+                      @Alias("exchange") String exchange) {
+            this.guildId = guildId;
+            this.channelId = channelId;
+            this.amqpUri = amqpUri;
+            this.exchange = exchange;
+        }
 
         @Ignore
         @Override
--- src/main/java/org/comroid/rabbitcord/RabbitCord.java
+++ src/main/java/org/comroid/rabbitcord/RabbitCord.java
@@ -1,33 +1,60 @@
 package org.comroid.rabbitcord;
 
-import jdk.jshell.JShell;
+import emoji4j.Emoji;
+import emoji4j.EmojiUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Value;
 import lombok.experimental.FieldDefaults;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.internal.entities.emoji.UnicodeEmojiImpl;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import org.comroid.annotations.Alias;
+import org.comroid.api.Polyfill;
 import org.comroid.api.data.seri.DataNode;
-import org.comroid.api.data.seri.JSON;
 import org.comroid.api.data.seri.Jackson;
+import org.comroid.api.func.util.Command;
 import org.comroid.api.func.util.Event;
+import org.comroid.api.func.util.Streams;
 import org.comroid.api.io.FileHandle;
+import org.comroid.api.text.Markdown;
+import org.comroid.api.text.TextDecoration;
+import org.comroid.api.tree.Component;
+import org.jetbrains.annotations.NotNull;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.kyori.adventure.text.Component.text;
 
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public enum RabbitCord {
+public enum RabbitCord implements Command.Handler {
     Instance;
 
     FileHandle DirBase;
@@ -35,6 +62,7 @@ public enum RabbitCord {
     FileHandle FileConfig;
     Config config;
     Event.Bus<GenericEvent> bus;
+    Command.Manager cmdr;
     JDA jda;
     Map<UUID, DiscordChannelConnection> channels;
 
@@ -47,12 +75,23 @@ public enum RabbitCord {
                     .asObject()
                     .convert(Config.class);
             bus = new Event.Bus<>();
+            cmdr = new Command.Manager(this);
+
             jda = JDABuilder.createLight(Objects.requireNonNull(config).token,
                             GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
                     .setCompression(Compression.ZLIB)
-                    .addEventListeners((EventListener) bus::publish)
+                    .addEventListeners((EventListener) bus::accept)
                     .build()
                     .awaitReady();
+            jda.updateCommands().addCommands(
+                    Commands.slash("link", "Link this channel to the specified AMQP Exchange")
+                            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL))
+                            .addOption(OptionType.STRING, "amqp-uri", "The AMQP URL to connect to", true)
+                            .addOption(OptionType.STRING, "exchange-name", "The AMQP exchange name to connect to", true),
+                    Commands.slash("status", "See AMQP channel status")
+                            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL))
+            ).queue();
+
             channels = Arrays.stream(Objects.requireNonNull(DirChannels.listFiles()))
                     .map(FileHandle::new)
                     .<DiscordChannelConnection.Config>map(file -> Jackson.JSON.parse(file.getContent(true))
@@ -60,9 +99,85 @@ public enum RabbitCord {
                             .convert(DiscordChannelConnection.Config.class))
                     .filter(Objects::nonNull)
                     .collect(Collectors.toMap(DiscordChannelConnection.Config::getUuid, DiscordChannelConnection::new));
+            channels.values().forEach(Component.Base::initialize);
+
+            bus.flatMap(SlashCommandInteractionEvent.class).listen()
+                    .subscribeData(event -> cmdr.execute(event.getName(), event, event.getUser(), event.getGuild(), event.getChannel()));
+            bus.flatMap(MessageReceivedEvent.class).listen().subscribeData(event -> {
+                var author = event.getAuthor();
+                if (author.isBot())
+                    return;
+
+                var channelId = new UUID(event.getGuild().getIdLong(), event.getChannel().getIdLong());
+                if (!channels.containsKey(channelId))
+                    return;
+
+                var message = event.getMessage();
+                var str = message.getContentStripped() + message.getAttachments().stream()
+                        .map(Message.Attachment::getUrl)
+                        .collect(Collectors.joining(" "," ",""));
+                //str = TextDecoration.sanitize(str, Markdown.class);
+
+                var comp = text("DISCORD ", TextColor.color(86, 98, 246))
+                        .append(text(EmojiUtils.removeAllEmojis(author.getEffectiveName()), TextColor.color(Objects.requireNonNull(message.getMember()).getColorRaw())))
+                        .append(text(": " + str, TextColor.color(0xFF_FF_FF)));
+                channels.get(channelId).sendToRabbit(comp);
+            });
         } catch (Throwable t) {
             throw new RuntimeException("Unable to start application", t);
         }
+    }
+
+    @Command(ephemeral = true)
+    public String link(SlashCommandInteractionEvent event, Guild guild, MessageChannelUnion channel) {
+        var uri = event.getOption("amqp-uri").getAsString();
+        var exchange = event.getOption("exchange-name").getAsString();
+        var config = new DiscordChannelConnection.Config(guild.getIdLong(), channel.getIdLong(), uri, exchange);
+        var id = config.getUuid();
+
+        DirChannels.createSubFile(id +".json").setContent(config.json());
+        var conn = new DiscordChannelConnection(config);
+        channels.put(id, conn);
+        conn.initialize();
+        return "Channel linked";
+    }
+
+    @Command(ephemeral = true)
+    public void status() {/*todo*/}
+
+    @Override
+    public void handleResponse(Command.Delegate cmd, @NotNull Object response, Object... args) {
+        final var e = Stream.of(args)
+                .flatMap(Streams.cast(SlashCommandInteractionEvent.class))
+                .findAny()
+                .orElseThrow();
+        final var user = Stream.of(args)
+                .flatMap(Streams.cast(User.class))
+                .findAny()
+                .orElseThrow();
+        if (response instanceof CompletableFuture)
+            e.deferReply().setEphemeral(cmd.ephemeral())
+                    .submit()
+                    .thenCombine(((CompletableFuture<?>) response), (hook, resp) -> {
+                        WebhookMessageCreateAction<Message> req;
+                        if (resp instanceof EmbedBuilder)
+                            req = hook.sendMessageEmbeds(embed((EmbedBuilder) resp, user).build());
+                        else req = hook.sendMessage(String.valueOf(resp));
+                        return req.submit();
+                    })
+                    .thenCompose(Function.identity())
+                    .exceptionally(Polyfill.exceptionLogger());
+        else {
+            ReplyCallbackAction req;
+            if (response instanceof EmbedBuilder)
+                req = e.replyEmbeds(embed((EmbedBuilder) response, user).build());
+            else req = e.reply(String.valueOf(response));
+            req.setEphemeral(cmd.ephemeral()).submit();
+        }
+    }
+
+    private EmbedBuilder embed(EmbedBuilder base, User user) {
+        return base.setAuthor(user.getName(), "https://forwardmq.comroid.org", user.getAvatarUrl());
     }
 
     public static void main(String[] args) {
@@ -72,5 +187,9 @@ public enum RabbitCord {
     @Value
     public static class Config implements DataNode {
         String token;
+
+        public Config(@Alias("token") String token) {
+            this.token = token;
+        }
     }
 }
