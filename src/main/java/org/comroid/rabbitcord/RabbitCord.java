785439ca2de185bcd1f8fb5121a17a4e7cf00e9a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-05-13 22:56:48 +0200

    omfg pls work

--- src/main/java/org/comroid/rabbitcord/RabbitCord.java
+++ src/main/java/org/comroid/rabbitcord/RabbitCord.java
@@ -108,8 +108,8 @@ public enum RabbitCord {
                     .subscribeData(event -> cmdr.execute(event.getName(), event, event.getUser(), event.getGuild(), event.getChannel()));
             bus.flatMap(MessageReceivedEvent.class).listen().subscribeData(event -> {
                 var author = event.getAuthor();
-                Log.at(Level.INFO, "Author ID: %d; Message: %s".formatted(author.getIdLong(), event.getMessage().getContentRaw()));
-                if (author.isBot() && author.getIdLong() == 1207401249922617445L)
+                //Log.at(Level.INFO, "Author ID: %d; Message: %s".formatted(author.getIdLong(), event.getMessage().getContentRaw()));
+                if (author.isBot() && (author.getIdLong() == 1207401249922617445L || author.getIdLong() == 1239680369217765466L))
                     return;
 
                 var channelId = new UUID(event.getGuild().getIdLong(), event.getChannel().getIdLong());
